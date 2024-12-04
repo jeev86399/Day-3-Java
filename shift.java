bcb689b152b11e8ea9608c33e81e67f31302cf59
jeev86399 <nnani86399@gmail.com>
2024-12-04 11:16:50 +0530

    Add files via upload

---
+++ shift.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class shift{
+    public static void main(String args[])
+    {
+        int a=23,b=2;
+        System.out.println("right shift:"+(a>>b));
+        System.out.println("left shift:"+(a<<b));
+    }
+}
