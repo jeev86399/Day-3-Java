364ec2ea011c7fe567093e197c1d63ab5c894c4b
jeev86399 <nnani86399@gmail.com>
2024-12-04 11:16:00 +0530

    Add files via upload

---
+++ powerbit.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class powerbit{
+    public static void main(String args[])
+    {
+        Scanner s=new Scanner(System.in);
+        int a=s.nextInt();
+        int b=1;
+        System.out.println(b<<a);
+    }
+}
